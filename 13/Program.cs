072c5416f71533c8856e8352672074c3868ed331
Alex Dedov <q.wwwww.q@inbox.ru>
2022-08-19 10:07:01 +0700

    Add 13

---
+++ 13/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.WriteLine("Введите Случайное число");
+int random =Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("третья цифра в числе");
+Console.WriteLine(random>99 ? random.ToString()[2]:"нет");
